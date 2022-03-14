2da094cfe4e5b725fc9b1d6949668d160f6e1bdd
Jorge Yakushi <jym990@gmail.com>
2022-03-14 02:51:45 -0500

    fixed start script

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,16 @@ import "dotenv/config";
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import boxRoutes from "./routes";
+import boxRoutes from "@/routes/index";
 
 const app: Express = express();
 
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
+
 app.use(boxRoutes);
+
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.k97cx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 console.log(uri);
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
